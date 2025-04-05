7b00ec261b90e80119374541002fe3da5bd3773d
Brauuwu <survivalminecraft080@gmail.com>
2025-04-05 13:21:40 +0700

    Update ktx_app.py

--- Streamlit/ktx_app.py
+++ Streamlit/ktx_app.py
@@ -9,7 +9,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
 
-logo_base64 = get_base64_image("Logo_PTIT.png")
+logo_base64 = get_base64_image("images/Logo_PTIT.png")
 
 def show_dashboard():
     # ----- Phần header với logo PTIT -----
